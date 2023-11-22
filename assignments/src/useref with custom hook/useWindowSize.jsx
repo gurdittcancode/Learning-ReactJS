e96aa71c0321ee09a8f01c814c38c2db97060146
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2023-11-22 23:45:42 +0530

    Moved assignments app to Ubuntu

--- assignments/src/useref with custom hook/useWindowSize.jsx
+++
@@ -1,6 +0,0 @@
-import { useRef } from "react";
-
-export function useWindowSize() {
-  const windowDimensions = useRef([window.innerWidth, window.innerHeight]);
-  return windowDimensions.current;
-}
